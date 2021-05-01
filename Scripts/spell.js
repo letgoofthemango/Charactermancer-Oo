ef2bc59e54742383197a9e9acc4de8f7ed79a11c
Daniel Pleschutznig <daniel@ple.org>
2021-05-01 20:26:35 +0200

    upload

---
+++ Scripts/spell.js
@@ -0,0 +1,26 @@
+const SpellSchool = {
+    ABJURATION: "Abjuration",
+    CONJURATION: "Conjuration",
+    DIVINATION: "Divination",
+    ENCHANTMENT: "Enchantment",
+    EVOCATION: "Evocation",
+    ILLUSION: "Illusion",
+    NECROMANCY: "Necromancy",
+    TRANSMUTATION: "Transmutation"
+}
+
+class Spell {
+    constructor(name, spellLevel, school, castingTime, range, components, duration, description, classes, atHigherLevels) {
+        this.name = name,
+            this.spellLevel = spellLevel,
+            this.school = school,
+            this.castingTime = castingTime,
+            this.range = range,
+            this.components = components,
+            this.duration = duration,
+            this.description = description,
+            this.classes = classes,
+            this.atHigherLevels = atHigherLevels,
+            this.known = false
+    }
+}
